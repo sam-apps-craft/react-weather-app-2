b83c2a38977d839d273527f864211029fd1075fc
sam1246 <sam@apps-craft.com>
2024-02-23 18:19:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react"
 import './App.css';
 
 export default function App() {
-  return <div className="App">
-    
-    Weather App 
-    
-    <footer>
-
-      This project was coded by Sam Raymond and is open-sourced on {" "}
-      <a href="https://github.com/sam-apps-craft/react-weather-app-2"
-      target="_blank"
-      >GitHub</a>
-    </footer>
-    
-    </div>;}
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Sam Raymond and is open-sourced on{" "}
+        <a
+          href="https://github.com/sam-apps-craft/react-weather-app-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );}
   
