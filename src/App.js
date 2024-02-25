599bdfcf990da55c94311ffba92656f53350d096
sam1246 <sam@apps-craft.com>
2024-02-25 13:09:58 +0000

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Sam Raymond and is open-sourced on{" "}
-        <a
-          href="https://github.com/sam-apps-craft/react-weather-app-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+
+        <Weather />
+        <footer>
+          This project was coded by Sam Raymond and is open-sourced on{" "}
+          <a
+            href="https://github.com/sam-apps-craft/react-weather-app-2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
-  );}
-  
+  );
+}
