f6a3ef35dd6aa459c80e56523cc2d5be0891340c
sam1246 <sam@apps-craft.com>
2024-02-26 17:03:10 +0000

    Corrected positioning

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-        <Weather defaultCity = "Tadcaster"/>
+        <Weather defaultCity="Tadcaster" />
         <footer>
           This project was coded by Sam Raymond and is open-sourced on{" "}
+          <a href="" target="_blank" rel="noreferrer">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
           <a
-            href="https://github.com/sam-apps-craft/react-weather-app-2"
+            href="https://react-weather-app-2-sam.netlify.app/"
             target="_blank"
             rel="noreferrer"
           >
-            GitHub
+            Netlify
           </a>
         </footer>
       </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,34 +1,35 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1> {props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
 
-    return (
-      <div className="WeatherInfo">
-        <h1> {props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
+      
 
-        <div className="row mt-3">
-          <div className="col-6">
-            <img src={props.data.iconUrl} alt={props.data.iconUrl} />
-
-<WeatherTemperature celsius={props.data.temperature} />
-
-          </div>
-
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity}% </li>
-              <li>Wind: {props.data.wind} mph</li>
-            </ul>
-          </div>
+      <div className="row mt-3">
+        <div className="col-2">
+          <img src={props.data.iconUrl} alt={props.data.iconUrl} />
+        </div>{" "}
+        <div className="col-3">
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+        <div className="col-7">
+          <ul className="details">
+            <li>Humidity: {props.data.humidity}% </li>
+            <li>Wind: {props.data.wind} mph</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
