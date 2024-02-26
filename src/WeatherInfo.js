2ac4af8cc29f792ae6fe205f0b4976f2177173ac
sam1246 <sam@apps-craft.com>
2024-02-26 16:09:59 +0000

    New component for C/F temperature conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
 
@@ -15,12 +16,10 @@ export default function WeatherInfo(props) {
 
         <div className="row mt-3">
           <div className="col-6">
-            <img src={props.data.iconUrl} alt={props.data.description} />
+            <img src={props.data.iconUrl} alt={props.data.iconUrl} />
 
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
+<WeatherTemperature celsius={props.data.temperature} />
+
           </div>
 
           <div className="col-6">
